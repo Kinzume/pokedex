bda29326f40d949e93313f09329a09aa434834bf
Kinzume <81969626+Kinzume@users.noreply.github.com>
2022-11-07 18:35:44 +0000

    fixed scrolling and scrollbar styling

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// Add Pokedex navigation controls
+const pokedex = document.querySelector(".pokedex");
+
+const controls = document.createElement("ol");
+controls.setAttribute("role", "list");
+controls.classList.add("pokedex-controls");
+controls.setAttribute("aria-label", "Navigation controls");
 /**
  * Returns the distance from the starting edge of the viewport to the given focal point on the element.
  * @param {HTMLElement} element
@@ -15,6 +22,75 @@ const getDistanceToFocalPoint = (element, focalPoint = "center") => {
       return rect.top + rect.height / 2;
   }
 };
+/**
+ * @param {'start'|'end'} direction
+ */
+const createNavButton = (direction) => {
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  button.classList.add(
+    "pokedex-control",
+    direction,
+    "triangle",
+    direction === "start" ? "up" : "down"
+  );
+  button.ariaLabel = direction === "start" ? "Previous" : "Next";
+  button.addEventListener("click", () => navigateToNextItem(direction));
+  li.appendChild(button);
+  controls.appendChild(li);
+  return button;
+};
+/**
+ * @param {'start'|'end'} direction
+ */
+function navigateToNextItem(direction) {
+  const imagesScrollContainer = document.querySelector(
+    '[class="images-scroll-container scroll-snap"]'
+  );
+  const imageList = imagesScrollContainer.querySelector('[role="list"]');
+  let mediaItems = Array.from(imageList.querySelectorAll(":scope > *"));
+  mediaItems = direction === "start" ? mediaItems.reverse() : mediaItems;
+  const scrollContainerCenter = getDistanceToFocalPoint(
+    imagesScrollContainer,
+    "center"
+  );
+  let targetFocalPoint;
+  for (const mediaItem of mediaItems) {
+    let focalPoint = window.getComputedStyle(mediaItem).scrollSnapAlign;
+    if (focalPoint === "none") {
+      focalPoint = "center";
+    }
+    const distanceToItem = getDistanceToFocalPoint(mediaItem, focalPoint);
+    // The 1s are to account for off-by-one errors. Sometimes, the currently centered
+    // media item's center doesn't match the pokedex's center and is off by one.
+    if (
+      (direction === "start" && distanceToItem + 1 < scrollContainerCenter) ||
+      (direction === "end" && distanceToItem - scrollContainerCenter > 1)
+    ) {
+      targetFocalPoint = distanceToItem;
+      break;
+    }
+  }
+  // This should never happen, but it doesn't hurt to check
+  if (typeof targetFocalPoint === "undefined") return;
+  const scrollAmount = targetFocalPoint - scrollContainerCenter;
+  imagesScrollContainer.scrollBy({
+    top: scrollAmount,
+    behavior: "smooth",
+  });
+}
+const navControlPrevious = createNavButton("start");
+const navControlNext = createNavButton("end");
+pokedex.appendChild(controls);
+
+const imagesScrollContainer = document.querySelector(
+  '[class="images-scroll-container"]'
+);
+const imageList = imagesScrollContainer.querySelector('[role="list"]');
+const namesScrollContainer = document.querySelector(
+  '[class="names-scroll-container"]'
+);
+const namesList = namesScrollContainer.querySelector('[role="list"]');
 
 /**
  * Returns a string with leading zeroes of the given number.
@@ -23,240 +99,193 @@ const getDistanceToFocalPoint = (element, focalPoint = "center") => {
 function addLeadingZeroes(entryNumber) {
   return ("000" + entryNumber).slice(-3);
 }
-const pokeball = document.querySelector('[class="pokeball"]');
-class Pokedex {
-  constructor(props) {
-    this.navigateToNextItem = this.navigateToNextItem.bind(this);
-    this.pokedex = props.root;
-    this.imagesScrollContainer = this.pokedex.querySelector(
-      '[class="images-scroll-container"]'
-    );
-    this.imageList = this.imagesScrollContainer.querySelector('[role="list"]');
 
-    this.namesScrollContainer = this.pokedex.querySelector(
-      '[class="names-scroll-container"]'
-    );
-    this.namesList = this.namesScrollContainer.querySelector('[role="list"]');
-
-    const controls = document.createElement("ol");
-    controls.setAttribute("role", "list");
-    controls.classList.add("pokedex-controls");
-    controls.setAttribute("aria-label", "Navigation controls");
-    /**
-     * @param {'start'|'end'} direction
-     */
-    const createNavButton = (direction) => {
-      const li = document.createElement("li");
-      const button = document.createElement("button");
-      button.classList.add(
-        "pokedex-control",
-        direction,
-        "triangle",
-        direction === "start" ? "up" : "down"
-      );
-      button.ariaLabel = direction === "start" ? "Previous" : "Next";
-      button.addEventListener("click", () =>
-        this.navigateToNextItem(direction)
-      );
-      li.appendChild(button);
-      controls.appendChild(li);
-      return button;
-    };
-    this.navControlPrevious = createNavButton("start");
-    this.navControlNext = createNavButton("end");
-    this.pokedex.appendChild(controls);
-
-    this.renderImages = this.renderImages.bind(this);
-    this.renderNames = this.renderNames.bind(this);
-    this.addSyncScroll = this.addSyncScroll.bind(this);
-    this._handlePokedexScroll = this._handlePokedexScroll.bind(this);
-    this.imagesScrollContainer.addEventListener("scroll", () => {
-      this._handlePokedexScroll();
-    });
-    this._rotatePokeball = this._rotatePokeball.bind(this);
-    this._rotatePokeball();
-
-    pokeball.style.transform = "scale(5) translate(-1em) rotate(90deg)";
-    const endpoint = fetch("https://pokeapi.co/api/v2/pokedex/4/");
-    endpoint
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((json) => {
-        this.renderImages(json);
-        this.renderNames(json);
-        this.addSyncScroll();
-        this._handlePokedexScroll();
-      });
-  }
-
-  /**
-   * @param {'start'|'end'} direction
-   */
-  navigateToNextItem(direction) {
-    let mediaItems = Array.from(this.imageList.querySelectorAll(":scope > *"));
-    mediaItems = direction === "start" ? mediaItems.reverse() : mediaItems;
-    const scrollContainerCenter = getDistanceToFocalPoint(
-      this.imagesScrollContainer,
-      "center"
-    );
-    let targetFocalPoint;
-    for (const mediaItem of mediaItems) {
-      let focalPoint = window.getComputedStyle(mediaItem).scrollSnapAlign;
-      if (focalPoint === "none") {
-        focalPoint = "center";
-      }
-      const distanceToItem = getDistanceToFocalPoint(mediaItem, focalPoint);
-      // The 1s are to account for off-by-one errors. Sometimes, the currently centered
-      // media item's center doesn't match the pokedex's center and is off by one.
-      if (
-        (direction === "start" && distanceToItem + 1 < scrollContainerCenter) ||
-        (direction === "end" && distanceToItem - scrollContainerCenter > 1)
-      ) {
-        targetFocalPoint = distanceToItem;
-        break;
-      }
-    }
-    // This should never happen, but it doesn't hurt to check
-    if (typeof targetFocalPoint === "undefined") return;
-    const scrollAmount = targetFocalPoint - scrollContainerCenter;
-    this.imagesScrollContainer.scrollBy({
-      top: scrollAmount,
-      behavior: "smooth",
-    });
-  }
-
-  _handlePokedexScroll() {
-    const imagesScrollTop = Math.abs(this.imagesScrollContainer.scrollTop);
-    // off-by-one correction for Chrome, where clientHeight is sometimes rounded down
-    const imagesHeight = this.imagesScrollContainer.clientHeight + 1;
-    const isAtStart = Math.floor(imagesScrollTop) <= 150;
-    const isAtEnd =
-      Math.ceil(imagesHeight + imagesScrollTop + 150) >=
-      this.imagesScrollContainer.scrollHeight;
-    this.navControlPrevious.setAttribute("aria-disabled", isAtStart);
-    this.navControlNext.setAttribute("aria-disabled", isAtEnd);
-  }
-  _rotatePokeball() {
-    const namesScrollContainer = SimpleBar.instances
-      .get(document.querySelector("[data-simplebar]"))
-      .getScrollElement();
-
-    let leftScrollSync = false;
-    let rightScrollSync = false;
-    const leftCol = this.imagesScrollContainer;
-    const rightCol = namesScrollContainer;
-    leftCol.addEventListener("scroll", () => {
-      if (!leftScrollSync) {
-        rightScrollSync = true;
-        const image = this.imageList.querySelector('[class="image-item"]');
-        // a mod b = ((a % b) + b) % b
-        const rotation = (leftCol.scrollTop / image.clientHeight) * 22.5;
-        const a = rotation;
-        const b = 360;
-        const amodb = ((a % b) + b) % b;
-        pokeball.style.transform = `scale(5) translate(-1em) rotate(${amodb}deg)`;
-      }
-      leftScrollSync = false;
-    });
-    rightCol.addEventListener("scroll", () => {
-      if (!rightScrollSync) {
-        leftScrollSync = true;
-        const name = this.namesList.querySelector('[class="name-item"]');
-        // a mod b = ((a % b) + b) % b
-        console.log(rightCol.scrollTop);
-        const rotation = (rightCol.scrollTop / name.clientHeight) * 22.5;
-        const a = rotation;
-        const b = 360;
-        const amodb = ((a % b) + b) % b;
-        console.log(amodb);
-        pokeball.style.transform = `scale(5) translate(-1em) rotate(${amodb}deg)`;
-      }
-      rightScrollSync = false;
-    });
-  }
-
-  renderImages(json) {
-    for (let i = 0; i < json["pokemon_entries"].length; i++) {
-      const speciesName = json["pokemon_entries"][i]["pokemon_species"]["name"];
-      const speciesUrl = json["pokemon_entries"][i]["pokemon_species"]["url"];
-      const id = speciesUrl.match(/[/][0-9]+/g)[0].slice(1);
-      const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald/${id}.png`;
-      const img = new Image();
-      img.loading = "lazy";
-      img.decoding = "async";
-      img.alt = speciesName;
-      img.src = spriteUrl;
-
-      const pokemonImage = document.createElement("li");
-      pokemonImage.className = "image-item";
-      pokemonImage.append(img);
-      this.imageList.append(pokemonImage);
-    }
-  }
-  renderNames(json) {
-    for (let i = 0; i < json["pokemon_entries"].length; i++) {
-      const speciesName = json["pokemon_entries"][i]["pokemon_species"]["name"];
-      const pokemonName = document.createElement("li");
-      pokemonName.className = "name-item";
-      const entryNumber = document.createElement("span");
-      entryNumber.className = "entry-number";
-      const pokeball = new Image();
-      pokeball.alt = "pokeball";
-      pokeball.src =
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png";
-      entryNumber.append(pokeball);
-      entryNumber.append("No" + addLeadingZeroes(i + 1) + " ");
-      entryNumber.setAttribute("aria-hidden", true);
-      pokemonName.append(entryNumber);
-      pokemonName.append(speciesName.toUpperCase());
-
-      this.namesList.append(pokemonName);
-    }
-  }
-
-  addSyncScroll() {
-    // Allows the user to simultaneously scroll two containers
-    const namesScrollContainer = SimpleBar.instances
-      .get(document.querySelector("[data-simplebar]"))
-      .getScrollElement();
-
-    let leftScrollSync = false;
-    let rightScrollSync = false;
-    const leftCol = this.imagesScrollContainer;
-    const rightCol = namesScrollContainer;
-    const leftMaxScrollTop = leftCol.scrollHeight - leftCol.clientHeight;
-    const rightMaxScrollTop = rightCol.scrollHeight - rightCol.clientHeight;
-
-    leftCol.addEventListener("scroll", () => {
-      if (!leftScrollSync) {
-        rightScrollSync = true;
-        const leftPercScrollTop = leftCol.scrollTop / leftMaxScrollTop;
-        const newRightColScrollTop = Math.ceil(
-          leftPercScrollTop * rightMaxScrollTop
-        );
-        rightCol.scrollTop = newRightColScrollTop;
-      }
-      leftScrollSync = false;
-    });
-
-    rightCol.addEventListener("scroll", () => {
-      if (!rightScrollSync) {
-        leftScrollSync = true;
-        const rightPercScrollTop = rightCol.scrollTop / rightMaxScrollTop;
-        const newLeftColScrollTop = Math.ceil(
-          rightPercScrollTop * leftMaxScrollTop
-        );
-        leftCol.scrollTop = newLeftColScrollTop;
-      }
-      rightScrollSync = false;
-    });
-  }
+function _handlePokedexScroll() {
+  const imagesScrollTop = Math.abs(imagesScrollContainer.scrollTop);
+  // off-by-one correction for Chrome, where clientHeight is sometimes rounded down
+  const imagesHeight = imagesScrollContainer.clientHeight + 1;
+  const isAtStart = Math.floor(imagesScrollTop) <= 100;
+  const isAtEnd =
+    Math.ceil(imagesHeight + imagesScrollTop + 100) >=
+    imagesScrollContainer.scrollHeight;
+  navControlPrevious.setAttribute("aria-disabled", isAtStart);
+  navControlNext.setAttribute("aria-disabled", isAtEnd);
 }
-
-const pokedex = new Pokedex({
-  root: document.querySelector(".pokedex"),
+imagesScrollContainer.addEventListener("scroll", () => {
+  _handlePokedexScroll();
 });
+
+const renderImages = (data) => {
+  const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald/`;
+  const ol = document.createElement("ol");
+  ol.className = "images";
+  ol.setAttribute("role", "list");
+  data.forEach(({ name, id }) => {
+    const li = document.createElement("li");
+    li.className = "image-item";
+    const img = new Image(150, 150);
+    img.alt = name;
+    img.src = imgUrl + `${id}.png`;
+    li.appendChild(img);
+    ol.append(li);
+  });
+  imagesScrollContainer.replaceChild(ol, imageList);
+  const newImages = document.querySelectorAll('[class="image-item"]>img');
+  const newImagesList = Array.from(newImages);
+  newImagesList[newImagesList.length - 1].onload = () => {
+    console.log("last image loaded");
+    newImages.forEach((img) => img.removeAttribute("height"));
+    const loader = document.querySelector('[class="loader"]');
+    console.log(loader);
+    loader.classList.add("loaded");
+    addSyncScroll();
+    _handlePokedexScroll();
+    _rotatePokeball();
+    setTimeout(() => {
+      loader.style = "z-index:-999";
+    }, 1200);
+  };
+};
+
+const renderNames = (data) => {
+  const ol = document.createElement("ol");
+  ol.className = "names flow";
+  ol.setAttribute("role", "list");
+  data.forEach(({ name }, i) => {
+    const li = document.createElement("li");
+    li.className = "name-item";
+    const span = document.createElement("span");
+    span.className = "entry-number";
+    span.setAttribute("aria-hidden", true);
+    const miniPokeball = new Image();
+    miniPokeball.alt = "mini pokeball";
+    miniPokeball.src =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png";
+    span.append(miniPokeball);
+    span.append("No" + addLeadingZeroes(i) + " ");
+    const li_content = document.createTextNode(name.toUpperCase());
+    li.appendChild(span);
+    li.appendChild(li_content);
+    ol.append(li);
+  });
+  namesScrollContainer
+    .querySelector('[class="simplebar-content"]')
+    .replaceChild(ol, namesList);
+};
+
+const pokeball = document.querySelector('[class="pokeball"]');
+function _rotatePokeball() {
+  const namesScrollContainer = SimpleBar.instances
+    .get(document.querySelector("[data-simplebar]"))
+    .getScrollElement();
+
+  let leftScrollSync = false;
+  let rightScrollSync = false;
+  const leftCol = imagesScrollContainer;
+  const rightCol = namesScrollContainer;
+  leftCol.addEventListener("scroll", () => {
+    if (!leftScrollSync) {
+      rightScrollSync = true;
+      const image = document.querySelector('[class="image-item"]');
+      // a mod b = ((a % b) + b) % b
+      const rotation = (leftCol.scrollTop / image.clientHeight) * 22.5;
+      const a = rotation;
+      const b = 360;
+      const amodb = ((a % b) + b) % b;
+      pokeball.style.transform = `scale(5) translate(-1em) rotate(${amodb}deg)`;
+    }
+    leftScrollSync = false;
+  });
+  rightCol.addEventListener("scroll", () => {
+    if (!rightScrollSync) {
+      leftScrollSync = true;
+      const name = document.querySelector('[class="name-item"]');
+      // a mod b = ((a % b) + b) % b
+      // console.log(rightCol.scrollTop);
+      const rotation = (rightCol.scrollTop / name.clientHeight) * 22.5;
+      const a = rotation;
+      const b = 360;
+      const amodb = ((a % b) + b) % b;
+      // console.log(amodb);
+      pokeball.style.transform = `scale(5) translate(-1em) rotate(${amodb}deg)`;
+    }
+    rightScrollSync = false;
+  });
+}
+function addSyncScroll() {
+  // Allows the user to simultaneously scroll two containers
+  const namesScrollContainer = SimpleBar.instances
+    .get(document.querySelector("[data-simplebar]"))
+    .getScrollElement();
+  let leftScrollSync = false;
+  let rightScrollSync = false;
+  const leftCol = document.querySelector(
+    '[class="images-scroll-container scroll-snap"]'
+  );
+  const oneImage = leftCol.querySelector('[class="image-item"]:nth-child(2)');
+  const rightCol = namesScrollContainer;
+  //   const leftMaxScrollTop = oneImage.clientHeight * 202;
+  //   const rightMaxScrollTop = rightCol.scrollHeight - rightCol.clientHeight;
+  //   const rightMaxScrollTop = oneName.clientHeight * 202;
+  //   console.log(rightMaxScrollTop);
+  leftCol.addEventListener("scroll", () => {
+    const leftMaxScrollTop = oneImage.clientHeight * 202;
+    const rightMaxScrollTop = rightCol.scrollHeight - rightCol.clientHeight;
+    if (!leftScrollSync) {
+      rightScrollSync = true;
+      const leftPercScrollTop = leftCol.scrollTop / leftMaxScrollTop;
+      const newRightColScrollTop = Math.ceil(
+        leftPercScrollTop * rightMaxScrollTop
+      );
+      rightCol.scrollTop = newRightColScrollTop;
+    }
+    leftScrollSync = false;
+  });
+
+  rightCol.addEventListener("scroll", () => {
+    const leftMaxScrollTop = oneImage.clientHeight * 202;
+    const rightMaxScrollTop = rightCol.scrollHeight - rightCol.clientHeight;
+    if (!rightScrollSync) {
+      leftScrollSync = true;
+      const rightPercScrollTop = rightCol.scrollTop / rightMaxScrollTop;
+      const newLeftColScrollTop = Math.ceil(
+        rightPercScrollTop * leftMaxScrollTop
+      );
+      leftCol.scrollTop = newLeftColScrollTop;
+    }
+    rightScrollSync = false;
+  });
+}
+pokeball.style.transform = "scale(5) translate(-1em) rotate(90deg)";
+const endpoint = fetch("https://pokeapi.co/api/v2/pokedex/4/");
+endpoint
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((json) => {
+    new Promise((resolve, reject) => {
+      const data = json["pokemon_entries"]
+        .map((entry) => {
+          return entry["pokemon_species"];
+        })
+        .map(({ name, url }) => {
+          const id = url.match(/[/][0-9]+/g)[0].slice(1);
+          return {
+            name,
+            id,
+          };
+        });
+      resolve(data);
+    }).then((data) => {
+      new Promise((resolve, reject) => {
+        renderImages(data);
+        renderNames(data);
+        resolve();
+      }).then(() => {
+        imagesScrollContainer.classList.add("scroll-snap");
+      });
+    });
+  });
